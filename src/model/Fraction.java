f40171a62c8f889e5d840029ea9c67cac5cd3b7a
Simon Stratemeier <sstratem@stud.hs-heilbronn.de>
2018-04-05 10:25:57 +0200

    move old max files to model

---
+++ src/model/Fraction.java
@@ -0,0 +1,129 @@
+package model;
+
+import java.math.BigInteger;
+
+/**
+ * @author Melanie Krugel 198991, Tobias Fetzer 198318, Simon Stratemeier 199067
+ * @version 2.0 08.01.2018
+ */
+public class Fraction extends Number implements Comparable<Fraction>{
+    public static final Fraction ZERO = new Fraction(0,1);
+    public static final Fraction DEFAULT = new Fraction(0,1);
+
+    private BigInteger numerator;
+    private BigInteger denominator;
+
+    public Fraction(BigInteger numerator, BigInteger denominator)  {
+        super();
+
+        if(denominator.equals(BigInteger.ZERO)) {
+            throw new DivisionByZeroException("denominator is zero");
+        }
+
+        if(denominator.compareTo(BigInteger.ZERO) < 0) {
+            numerator = numerator.negate();
+            denominator = denominator.negate();
+        }
+
+        BigInteger gcd = numerator.gcd(denominator);
+        this.numerator = numerator.divide(gcd);
+        this.denominator = denominator.divide(gcd);
+    }
+
+    public Fraction(int numerator, int denominator) {
+        this(
+                BigInteger.valueOf(numerator),
+                BigInteger.valueOf(denominator)
+        );
+    }
+
+    public Fraction(BigInteger numerator) {
+        this.numerator = numerator;
+        this.denominator = BigInteger.ONE;
+    }
+
+    Fraction add(Fraction r) throws DivisionByZeroException{
+        BigInteger numerator = this.getNumerator().multiply(r.getDenominator())
+                .add(r.getNumerator().multiply(this.getDenominator()));
+        BigInteger denominator = this.getDenominator().multiply(r.getDenominator());
+        return new Fraction(numerator, denominator);
+    }
+
+    Fraction subtract(Fraction r) throws DivisionByZeroException {
+        BigInteger numerator = this.getNumerator().multiply(r.getDenominator())
+                .subtract(r.getNumerator().multiply(this.getDenominator()));
+        BigInteger denominator = this.getDenominator().multiply(r.getDenominator());
+        return new Fraction(numerator, denominator);
+    }
+
+    Fraction multiply(Fraction r) {
+        return new Fraction(
+                this.getNumerator().multiply(r.getNumerator()),
+                this.getDenominator().multiply(r.getDenominator())
+        );
+    }
+
+    Fraction divide(Fraction r) throws DivisionByZeroException {
+        return new Fraction(
+                this.getNumerator().divide(r.getNumerator()),
+                this.getDenominator().divide(r.getDenominator())
+        );
+    }
+
+    public String toString(){
+        if(numerator.equals(BigInteger.ONE)){
+            return denominator.toString();
+        }
+        long num = getNumerator().longValue();
+        long det = getDenominator().longValue();
+        return num + "/" + det;
+    }
+
+    boolean isInteger() {
+        return getDenominator().equals(BigInteger.ONE);
+    }
+
+    public BigInteger getNumerator() {
+        return this.numerator;
+    }
+
+    public BigInteger getDenominator() {
+        return this.denominator;
+    }
+
+
+    public int compareTo(Fraction frac) {
+        BigInteger t = this.getNumerator().multiply(frac.getDenominator());
+        BigInteger f = frac.getNumerator().multiply(this.getDenominator());
+
+        return t.compareTo(f);
+    }
+
+    public boolean equals(Object obj) {
+        return compareTo((Fraction)obj) == 0;
+    }
+
+    public BigInteger bigIntValue() { return numerator.divide(denominator); }
+
+    public double doubleValue() {
+        return (numerator.doubleValue())/(denominator.doubleValue());
+    }
+
+    public float floatValue() {
+        return (float) this.doubleValue();
+    }
+
+    public int intValue() {
+        return (int) this.doubleValue();
+    }
+
+    public long longValue() {
+        return (long) this.doubleValue();
+    }
+}
+
+class DivisionByZeroException extends Error {
+    DivisionByZeroException(String message) {
+        super(message);
+    }
+}
---
+++ src/model/MAX.java
@@ -0,0 +1,163 @@
+package model;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+/**
+ * MAX Game
+ * @author Melanie Krugel 198991, Tobias Fetzer 198318, Simon Stratemeier 199067
+ * @version 2.0 08.01.2018
+ */
+public class MAX {
+    // String colors
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+
+    // Variables for playing ground properties
+    public static final int END_X = 8;
+    public static final int END_Y = 8;
+    public static final int START_X = 1;
+    public static final int START_Y = 1;
+    public static final Fraction SCORE_TARGET = new Fraction(80, 1);
+    public static Fraction sum = new Fraction(0,1);
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        Boolean end = false;
+        Matrix<Fraction> mat = initMatrix();
+        Player player1 = new Player(new Position(4, 4), "red", ANSI_RED + "R" + ANSI_RESET);
+        Player player2 = new Player(new Position(5, 5), "green", ANSI_GREEN + "G" + ANSI_RESET);
+        Player currentPlayer = player1;
+        Player otherPlayer = player2;
+
+        // Inital draw
+        Board.draw(player1, player2, currentPlayer, mat);
+        line="";
+        while (!end&&!line.equalsIgnoreCase("quit")) {
+            // Keyboard command
+            line = in.readLine();
+
+            // Loop for next round or quit
+            while (!line.equalsIgnoreCase("quit")) {
+                // Move current player if movement in given direction is allowed
+                if (line.equalsIgnoreCase("W")
+                        && currentPlayer.position.y > START_Y
+                        && !isSamePosition(currentPlayer.peekDirection(Direction.UP), otherPlayer)) {
+                    currentPlayer.moveDirection(Direction.UP);
+                    break;
+                }
+                if (line.equalsIgnoreCase("A")
+                        && currentPlayer.position.x > START_X
+                        && !isSamePosition(currentPlayer.peekDirection(Direction.LEFT), otherPlayer)) {
+                    currentPlayer.moveDirection(Direction.LEFT);
+                    break;
+                }
+                if (line.equalsIgnoreCase("S")
+                        && currentPlayer.position.y < END_Y
+                        && !isSamePosition(currentPlayer.peekDirection(Direction.DOWN), otherPlayer)) {
+                    currentPlayer.moveDirection(Direction.DOWN);
+                    break;
+                }
+                if (line.equalsIgnoreCase("D")
+                        && currentPlayer.position.x < END_X
+                        && !isSamePosition(currentPlayer.peekDirection(Direction.RIGHT), otherPlayer)) {
+                    currentPlayer.moveDirection(Direction.RIGHT);
+                    break;
+                }
+
+                // Border reached or unknown command
+                System.out.println("Nicht möglich! Neu eingeben");
+                // Retry input
+                line = in.readLine();
+            }
+
+            // Update player score
+            currentPlayer.score = currentPlayer.score.add(mat.getValue(currentPlayer.position.x, currentPlayer.position.y));
+            // Update score of remaining playing field points
+            sum = sum.subtract(mat.getValue(currentPlayer.position.x, currentPlayer.position.y));
+            // When player arrives field set field value to 0
+            mat.setValue(currentPlayer.position.x, currentPlayer.position.y, Fraction.ZERO);
+
+            // Rotate current player
+            if (currentPlayer == player1) {
+                currentPlayer = player2;
+                otherPlayer = player1;
+            } else {
+                currentPlayer = player1;
+                otherPlayer = player2;
+            }
+
+            // Update console output
+            if(!line.equalsIgnoreCase("quit")) {
+                Board.draw(player1, player2, currentPlayer, mat);
+            }
+            else{
+                System.out.println("Game cancelled");
+            }
+            // Announce winner
+            if (player1.score.compareTo(SCORE_TARGET) >= 1) {
+                System.out.println(player1.getName() + " wins!" );
+                end = true;
+            }
+            if (player2.score.compareTo(SCORE_TARGET) >= 1) {
+                System.out.println(player2.getName() + " wins!");
+                end = true;
+            }
+            // Announce tie
+            if(sum.equals(Fraction.ZERO)){
+                int i=player1.score.compareTo(player2.score);
+                if(i==0) System.out.println("Unentschieden");
+                else {
+                    System.out.println(i == 1 ? player1.getName() + " wins!" : player2.getName() + " wins!");
+                }
+                end=true;
+            }
+
+        }
+
+        // Close buffered reader
+        in.close();
+    }
+
+    // Check if players have the same position
+    public static Boolean isSamePosition(Player p1, Player p2) {
+        return p1.position.equals(p2.position);
+    }
+
+    // Create Fraction with random (more or less) value
+    public static Fraction getGameFraction() {
+        int minNumerator, maxNumerator, randomNumerator;
+        int minDenominator, maxDenominator, randomDenominator;
+
+        minNumerator = 10;
+        maxNumerator = 99;
+        randomNumerator = MathUtil.randomRange(minNumerator, maxNumerator);
+
+        minDenominator = randomNumerator / 10;
+        maxDenominator = randomNumerator;
+        randomDenominator = MathUtil.randomRange(minDenominator, maxDenominator);
+
+        Fraction frac = new Fraction(randomNumerator, randomDenominator);
+        sum = sum.add(frac);
+        return frac;
+    }
+
+
+    // create Matrix representing the playing field
+    public static Matrix<Fraction> initMatrix() {
+        int rows = END_X - START_X + 1;
+        int columns = END_Y - START_Y + 1;
+
+        Matrix<Fraction> mat = new Matrix<>(rows, columns, Fraction.DEFAULT);
+
+        mat.map(MAX::getGameFraction);
+
+        // Output sum for debugging purposes
+        //System.out.println(mat.reduce((acc, curr) -> acc.add(curr), Fraction.DEFAULT).intValue());
+        return mat;
+    }
+}
