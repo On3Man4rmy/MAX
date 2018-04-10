44d2ed165e5a9159e61b573b64deb01f03de99f1
Tobias <tobias.fetzer97@googlemail.com>
2018-04-10 20:11:17 +0100

    Multiple games can now be individually controlled

--- src/MAXAPP.java
+++ src/MAXAPP.java
@@ -1,3 +1,4 @@
+import controller.RootLayoutController;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -5,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import util.KeyboardEventPublisher;
 
@@ -14,19 +16,26 @@ public class MAXAPP extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Button btOK = new Button("OK");
+        Button btOK = new Button("Start Game");
         btOK.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 Stage stage = new Stage();
-                Parent root = null;
+                Parent root=null;
+                RootLayoutController controller=null;
                 try {
-                    root = FXMLLoader.load(getClass().getResource("view/RootLayout.fxml"));
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource(
+                            "view/RootLayout.fxml"));
+                    root =loader.load();
+                    controller=loader.getController();
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
+
                 Scene scene = new Scene(root, 500, 500);
-                scene.setOnKeyPressed(root.::publish);
+                scene.setOnKeyPressed(controller.keyboardEventPublisher::publish);
                 stage.setScene(scene);
                 stage.setTitle("MAX Game");
                 stage.show();
