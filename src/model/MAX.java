d72b1717b8072886ab3844ed296230c27b4dd8d4
Tobias <tobias.fetzer97@googlemail.com>
2018-04-10 10:49:36 +0100

    removed out commented code. We have version control for a reason changed var to Boardelement, so I can use it with Java 1.8 Player doesn't change when walking into wall

--- src/model/MAX.java
+++ src/model/MAX.java
@@ -16,10 +16,6 @@ import static javafx.application.Application.launch;
  */
 
 public class MAX {
-    // String colors
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
 
     // Variables for playing ground properties
     public static final int END_X = 8;
@@ -34,6 +30,7 @@ public class MAX {
     Player currentPlayer = player1;
     Player otherPlayer = player2;
     public Board board = new Board();
+    public boolean changePlayer =true;
 
     public MAX() {
         board.update(player1, player2, currentPlayer, mat);
@@ -41,26 +38,28 @@ public class MAX {
     }
 
     public void enterAction(Actions action) {
+
         if (action == Actions.UP
                 && currentPlayer.position.y > START_Y
                 && !isSamePosition(currentPlayer.peekDirection(Direction.UP), otherPlayer)) {
             currentPlayer.moveDirection(Direction.UP);
         }
-        if (action == Actions.LEFT
+        else if (action == Actions.LEFT
                 && currentPlayer.position.x > START_X
                 && !isSamePosition(currentPlayer.peekDirection(Direction.LEFT), otherPlayer)) {
             currentPlayer.moveDirection(Direction.LEFT);
         }
-        if (action == Actions.DOWN
+        else if (action == Actions.DOWN
                 && currentPlayer.position.y < END_Y
                 && !isSamePosition(currentPlayer.peekDirection(Direction.DOWN), otherPlayer)) {
             currentPlayer.moveDirection(Direction.DOWN);
         }
-        if (action == Actions.RIGHT
+       else if (action == Actions.RIGHT
                 && currentPlayer.position.x < END_X
                 && !isSamePosition(currentPlayer.peekDirection(Direction.RIGHT), otherPlayer)) {
             currentPlayer.moveDirection(Direction.RIGHT);
         }
+        else{changePlayer=false;}
 
         // Update player score
         currentPlayer.setScore(currentPlayer.getScore().add(mat.getValue(currentPlayer.position.x, currentPlayer.position.y)));
@@ -70,16 +69,18 @@ public class MAX {
         mat.setValue(currentPlayer.position.x, currentPlayer.position.y, Fraction.ZERO);
 
         // Rotate current player
-        if (currentPlayer == player1) {
-            currentPlayer = player2;
-            otherPlayer = player1;
-        } else {
-            currentPlayer = player1;
-            otherPlayer = player2;
+        if(changePlayer) {
+            if (currentPlayer == player1) {
+                currentPlayer = player2;
+                otherPlayer = player1;
+            } else {
+                currentPlayer = player1;
+                otherPlayer = player2;
+            }
+            currentPlayer.setIsSelectedProperty(true);
+            otherPlayer.setIsSelectedProperty(false);
         }
-        currentPlayer.setIsSelectedProperty(true);
-        otherPlayer.setIsSelectedProperty(false);
-
+        else{changePlayer=true;}
         // Announce winner
         if (player1.getScore().compareTo(SCORE_TARGET) >= 1) {
             System.out.println(player1.getName() + " wins!" );
@@ -98,101 +99,6 @@ public class MAX {
 
         board.update(player1, player2, currentPlayer, mat);
     }
-/*
-    public static void main(String[] args) throws IOException {
-        String line;
-        Boolean end = false;
-        Matrix<Fraction> mat = initMatrix();
-        Player player1 = new Player(new Position(4, 4), "red", ANSI_RED + "R" + ANSI_RESET);
-        Player player2 = new Player(new Position(5, 5), "green", ANSI_GREEN + "G" + ANSI_RESET);
-
-        // Inital draw
-        Board.draw(player1, player2, currentPlayer, mat);
-        line="";
-        while (!end&&!line.equalsIgnoreCase("quit")) {
-            // Keyboard command
-            line = in.readLine();
-
-            // Loop for next round or quit
-            while (!line.equalsIgnoreCase("quit")) {
-                // Move current player if movement in given direction is allowed
-                if (line.equalsIgnoreCase("W")
-                        && currentPlayer.position.y > START_Y
-                        && !isSamePosition(currentPlayer.peekDirection(Direction.UP), otherPlayer)) {
-                    currentPlayer.moveDirection(Direction.UP);
-                    break;
-                }
-                if (line.equalsIgnoreCase("A")
-                        && currentPlayer.position.x > START_X
-                        && !isSamePosition(currentPlayer.peekDirection(Direction.LEFT), otherPlayer)) {
-                    currentPlayer.moveDirection(Direction.LEFT);
-                    break;
-                }
-                if (line.equalsIgnoreCase("S")
-                        && currentPlayer.position.y < END_Y
-                        && !isSamePosition(currentPlayer.peekDirection(Direction.DOWN), otherPlayer)) {
-                    currentPlayer.moveDirection(Direction.DOWN);
-                    break;
-                }
-                if (line.equalsIgnoreCase("D")
-                        && currentPlayer.position.x < END_X
-                        && !isSamePosition(currentPlayer.peekDirection(Direction.RIGHT), otherPlayer)) {
-                    currentPlayer.moveDirection(Direction.RIGHT);
-                    break;
-                }
-
-                // Border reached or unknown command
-                System.out.println("Nicht möglich! Neu eingeben");
-                // Retry input
-                line = in.readLine();
-            }
-
-            // Update player score
-            currentPlayer.score = currentPlayer.score.add(mat.getValue(currentPlayer.position.x, currentPlayer.position.y));
-            // Update score of remaining playing field points
-            sum = sum.subtract(mat.getValue(currentPlayer.position.x, currentPlayer.position.y));
-            // When player arrives field set field value to 0
-            mat.setValue(currentPlayer.position.x, currentPlayer.position.y, Fraction.ZERO);
-
-            // Rotate current player
-            if (currentPlayer == player1) {
-                currentPlayer = player2;
-                otherPlayer = player1;
-            } else {
-                currentPlayer = player1;
-                otherPlayer = player2;
-            }
-
-            // Update console output
-            if(!line.equalsIgnoreCase("quit")) {
-                Board.draw(player1, player2, currentPlayer, mat);
-            else{
-                System.out.println("Game cancelled");
-            }
-            // Announce winner
-            if (player1.score.compareTo(SCORE_TARGET) >= 1) {
-                System.out.println(player1.getName() + " wins!" );
-                end = true;
-            }
-            if (player2.score.compareTo(SCORE_TARGET) >= 1) {
-                System.out.println(player2.getName() + " wins!");
-                end = true;
-            }
-            // Announce tie
-            if(sum.equals(Fraction.ZERO)){
-                int i=player1.score.compareTo(player2.score);
-                if(i==0) System.out.println("Unentschieden");
-                else {
-                    System.out.println(i == 1 ? player1.getName() + " wins!" : player2.getName() + " wins!");
-                }
-                end=true;
-            }
-
-        }
-
-        // Close buffered reader
-        in.close();
-    }*/
 
     public void run(){
                launch();
